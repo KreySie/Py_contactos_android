880a390cf3f26ad548f29834529dc675284cc523
FlutLab.io <>
2021-05-06 19:55:26 +0000

    Initial commit

---
+++ lib/datos_contacto.dart
@@ -0,0 +1,18 @@
+class Contacto {
+  final String nombrecompleto;
+  final String email;
+  const Contacto({this.nombrecompleto, this.email});
+} // fin de la clase contacto
+
+const loscontactos = <Contacto>[
+  Contacto(nombrecompleto: 'Luhi Hernandez', email: 'luhihernandez07@gmail.com'),
+  Contacto(nombrecompleto: 'Gael Rodriguez', email: 'Gaelin@gmail.com'),
+  Contacto(nombrecompleto: 'Lehi Hernandez', email: 'Lehihernandez07@gmail.com'),
+  Contacto(nombrecompleto: 'Manuel Valenzuela', email: 'ElValeBot@gmail.com'),
+  Contacto(nombrecompleto: 'Farid Castillo', email: 'guason@gmail.com'),
+  Contacto(nombrecompleto: 'Aurora Alvarez', email: 'LaAuroris@gmail.com'),
+  Contacto(nombrecompleto: 'Evelyn Valles', email: 'LaEveeeelyn@gmail.com'),
+  Contacto(nombrecompleto: 'Gerson Ramirez', email: 'Gersin@gmail.com'),
+  Contacto(nombrecompleto: 'Raquel Martinez', email: 'LaDalia@gmail.com'),
+  Contacto(nombrecompleto: 'Brisa Contreras', email: 'BrisaBrisin@gmail.com'),
+]; //final de <Contacto>[]
